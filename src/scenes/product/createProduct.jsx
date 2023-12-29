3f34a9edc31939573068c656d96dad88f6e29ca8
achraflafkiri <achraf.lafkiri.2@gmail.com>
2023-12-28 22:26:59 -0800

    s

--- src/scenes/product/createProduct.jsx
+++ src/scenes/product/createProduct.jsx
@@ -1,206 +1,168 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { getAllCategories } from "../../api/functions/categories";
 import {
-  Box,
-  Button,
-  TextField,
-  Select,
-  MenuItem,
   FormControl,
   InputLabel,
+  Input,
+  TextareaAutosize,
+  Select,
+  MenuItem,
+  Button,
+  Box,
 } from "@mui/material";
-import { Formik } from "formik";
-import * as yup from "yup";
-import useMediaQuery from "@mui/material/useMediaQuery";
 import Header from "../../components/Header";
-import { createProduct } from "../../api/functions/products";
-import { getAllCategories } from "../../api/functions/categories";
-import { useEffect, useState } from "react";
 
-const CreateProduct = () => {
-  const isNonMobile = useMediaQuery("(min-width:600px)");
+const ProductForm = () => {
+  const [formData, setFormData] = useState({
+    titleEN: "",
+    descriptionEN: "",
+    price: 0,
+    categoryId: "",
+    image: null,
+  });
 
-  const [categoriesData, setCategoriesData] = useState([]);
+  const [categoriesList, setCategoriesList] = useState([]);
 
   useEffect(() => {
-    const fetchCategories = async () => {
-      try {
-        const categoriesList = await getAllCategories();
-
-        if (categoriesList.data) {
-          setCategoriesData(categoriesList.data.data.categories);
-          // console.log(categoriesList.data.data.categories);
-        }
-      } catch (error) {
-        console.log(error);
+    const fetchData = async () => {
+      const catList = await getAllCategories();
+      if (catList.data) {
+        setCategoriesList(catList.data.data.categories);
       }
     };
-    fetchCategories();
-  }, []);
+    fetchData();
+  }, []); // Add an empty dependency array to run the effect only once
 
-  const handleFormSubmit = async (values) => {
-    console.log("values => ", values);
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    setFormData({
+      ...formData,
+      image: file,
+    });
+  };
+
+  const handleCategoryChange = (e) => {
+    const categoryId = e.target.value;
+    setFormData({
+      ...formData,
+      categoryId,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    console.log("values => ", formData);
 
     try {
-      const res = await createProduct(values);
+      const formDataToSend = new FormData();
+      formDataToSend.append("titleEN", formData.titleEN);
+      formDataToSend.append("descriptionEN", formData.descriptionEN);
+      formDataToSend.append("price", formData.price);
+      formDataToSend.append("categoryId", formData.categoryId);
+      formDataToSend.append("image", formData.image);
 
-      if (res.data) {
-        console.log("res data => ", res.data);
-      }
+      const response = await axios.post(
+        "http://localhost:8080/api/v1/products",
+        formDataToSend,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+
+      console.log("Response:", response.data);
+      // You can add logic here for handling the response if needed
     } catch (error) {
-      console.error("Error creating product:", error);
+      console.error("Error submitting form:", error);
+      // Handle error appropriately
     }
   };
 
   return (
     <Box m="20px">
-      <Header title="CREATE PRODUCT" subtitle="Create a New Product Profile" />
+      <Header title="CREATE PRODUCT" subtitle="C" />
 
-      <Formik
-        onSubmit={handleFormSubmit}
-        initialValues={initialValues}
-        validationSchema={checkoutSchema}
-      >
-        {({
-          values,
-          errors,
-          touched,
-          handleBlur,
-          handleChange,
-          handleSubmit,
-          setFieldValue,
-        }) => (
-          <form onSubmit={handleSubmit}>
-            <Box
-              display="grid"
-              gap="30px"
-              gridTemplateColumns="repeat(4, minmax(0, 1fr))"
-              sx={{
-                "& > div": { gridColumn: isNonMobile ? undefined : "span 4" },
-              }}
-            >
-              <TextField
-                fullWidth
-                variant="filled"
-                type="text"
-                label="Title"
-                onBlur={handleBlur}
-                onChange={handleChange}
-                value={values.title}
-                name="title"
-                error={!!touched.title && !!errors.title}
-                helperText={touched.title && errors.title}
-                sx={{ gridColumn: "span 4" }}
-              />
-              <TextField
-                fullWidth
-                variant="filled"
-                type="text"
-                label="Description"
-                onBlur={handleBlur}
-                onChange={handleChange}
-                value={values.description}
-                name="description"
-                error={!!touched.description && !!errors.description}
-                helperText={touched.description && errors.description}
-                sx={{ gridColumn: "span 4" }}
-              />
-              <TextField
-                fullWidth
-                variant="filled"
-                type="number"
-                label="Price"
-                onBlur={handleBlur}
-                onChange={handleChange}
-                value={values.price}
-                name="price"
-                error={!!touched.price && !!errors.price}
-                helperText={touched.price && errors.price}
-                sx={{ gridColumn: "span 4" }}
-              />
-              <FormControl
-                fullWidth
-                variant="filled"
-                sx={{ gridColumn: "span 4" }}
-              >
-                <InputLabel>Category</InputLabel>
-                <Select
-                  label="Category"
-                  onBlur={handleBlur}
-                  onChange={handleChange}
-                  value={values.category}
-                  name="category"
-                  error={!!touched.category && !!errors.category}
-                >
-                  {/* <MenuItem value="salon">Salon</MenuItem>
-                  <MenuItem value="lamp">Lamp</MenuItem>
-                  <MenuItem value="livingRoom">Living Room</MenuItem> */}
-
-                  {categoriesData &&
-                    categoriesData.map((item, index) => (
-                      <MenuItem value={item.name} key={index}>
-                        {item.name}
-                      </MenuItem>
-                    ))}
-                </Select>
-              </FormControl>
-
-              {/* Image Upload Input */}
-              <input
-                accept="image/*"
-                style={{ display: "none" }}
-                id="image-upload"
-                type="file"
-                onChange={(event) => {
-                  setFieldValue(
-                    "image",
-                    URL.createObjectURL(event.target.files[0])
-                  );
-                }}
-              />
-              <label htmlFor="image-upload">
-                <Button component="span" color="primary" variant="contained">
-                  Upload Image
-                </Button>
-              </label>
-              {values.image && (
-                <img
-                  src={values.image}
-                  alt="Preview"
-                  style={{ maxWidth: "100%", marginTop: "10px" }}
-                />
-              )}
-            </Box>
-            <Box display="flex" justifyContent="end" mt="20px">
-              <Button type="submit" color="secondary" variant="contained">
-                Create New Product
-              </Button>
-            </Box>
-          </form>
-        )}
-      </Formik>
+      <form onSubmit={handleSubmit}>
+        <FormControl>
+          <InputLabel htmlFor="titleEN">Title (EN):</InputLabel>
+          <Input
+            type="text"
+            id="titleEN"
+            name="titleEN"
+            value={formData.titleEN}
+            onChange={handleInputChange}
+          />
+        </FormControl>
+        <br />
+        <FormControl>
+          <InputLabel htmlFor="descriptionEN">Description (EN):</InputLabel>
+          <TextareaAutosize
+            id="descriptionEN"
+            name="descriptionEN"
+            value={formData.descriptionEN}
+            onChange={handleInputChange}
+          />
+        </FormControl>
+        <br />
+        <FormControl>
+          <InputLabel htmlFor="price">Price:</InputLabel>
+          <Input
+            type="number"
+            id="price"
+            name="price"
+            value={formData.price}
+            onChange={handleInputChange}
+          />
+        </FormControl>
+        <br />
+        <FormControl>
+          <InputLabel htmlFor="categoryId">Category:</InputLabel>
+          <Select
+            id="categoryId"
+            className="form-select"
+            label="Category"
+            value={formData.categoryId}
+            onChange={handleCategoryChange}
+          >
+            <MenuItem value="" disabled>
+              Select a category
+            </MenuItem>
+            {categoriesList &&
+              categoriesList.map((item, index) => (
+                <MenuItem value={item.id} key={index}>
+                  {item.name}
+                </MenuItem>
+              ))}
+          </Select>
+        </FormControl>
+        <br />
+        <FormControl>
+          <InputLabel htmlFor="image">Image:</InputLabel>
+          <Input
+            type="file"
+            id="image"
+            name="image"
+            onChange={handleImageChange}
+          />
+        </FormControl>
+        <br />
+        <Button variant="contained" type="submit">
+          Submit
+        </Button>
+      </form>
     </Box>
   );
 };
 
-const phoneRegExp =
-  /^((\+[1-9]{1,4}[ -]?)|(\([0-9]{2,3}\)[ -]?)|([0-9]{2,4})[ -]?)*?[0-9]{3,4}[ -]?[0-9]{3,4}$/;
-
-const checkoutSchema = yup.object().shape({
-  firstName: yup.string().required("required"),
-  lastName: yup.string().required("required"),
-  email: yup.string().email("invalid email").required("required"),
-  contact: yup
-    .string()
-    .matches(phoneRegExp, "Phone number is not valid")
-    .required("required"),
-  address1: yup.string().required("required"),
-  address2: yup.string().required("required"),
-});
-const initialValues = {
-  title: "",
-  description: "",
-  price: 0,
-  category: "",
-  image: "",
-};
-
-export default CreateProduct;
+export default ProductForm;
